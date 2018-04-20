2afc78ea694aaa91f4e550d8c857ce2e5bfa61e4
Mr-Abdali <sajidullah_m@yahoo.com>
2018-04-20 12:38:17 -0700

    Initial commit

---
+++ app/src/main/java/com/mr_abdali/tablayout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mr_abdali.tablayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        toolbar = (Toolbar) findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new OneFragment(), "ITEM ONE");
+        adapter.addFragment(new TwoFragment(), "ITEM TWO");
+        adapter.addFragment(new ThreeFragment(), "ITEM THREE");
+
+        viewPager.setAdapter(adapter);
+
+        //tablayout object
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
